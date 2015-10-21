c81105fd231b658a27403317741e2be3b6525910
Philipp Krenn <pk@xeraa.net>
2015-10-21 04:10:28 +0200

    ADDED more examples

---
+++ 01/src/main/java/net/xeraa/model/Name.java
@@ -0,0 +1,15 @@
+package net.xeraa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Name {
+
+  private final long id;
+  private final String name;
+
+}
