49d6a91d3126688c3b3c45471f38d9045e0bf25d
Philipp Krenn <pk@xeraa.net>
2015-10-22 07:22:23 +0200

    ADDED initial test

--- 05/src/test/java/net/xeraa/RestTrainingApplicationTests.java
+++ 05/src/test/java/net/xeraa/RestTrainingApplicationTests.java
@@ -1,18 +1,55 @@
 package net.xeraa;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentation;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = RestTrainingApplication.class)
 @WebAppConfiguration
 public class RestTrainingApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  protected static final String BOOK_URL = "/v2/book";
+
+  @Rule
+  public final RestDocumentation
+      restDocumentation = new RestDocumentation("build/generated-snippets");
+
+  @Autowired
+  private WebApplicationContext context;
+
+  private MockMvc mockMvc;
+
+  @Before
+  public void setUp() {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+	.apply(documentationConfiguration(this.restDocumentation))
+	.build();
+  }
+
+  @Test
+  public void emptyStart() throws Exception {
+    mockMvc.perform(get(BOOK_URL).accept(MediaType.APPLICATION_JSON))
+	.andExpect(status().isOk())
+	.andExpect(jsonPath("$", hasSize(0)))
+        .andDo(document("index"));
+  }
 
 }
