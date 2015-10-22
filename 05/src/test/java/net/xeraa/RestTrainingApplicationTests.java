8bc338fec5d7a4e36a2f5be4eb70cf48a279100b
Philipp Krenn <pk@xeraa.net>
2015-10-22 07:50:34 +0200

    ADDED more tests

--- 05/src/test/java/net/xeraa/RestTrainingApplicationTests.java
+++ 05/src/test/java/net/xeraa/RestTrainingApplicationTests.java
@@ -1,5 +1,9 @@
 package net.xeraa;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.xeraa.model.Book2;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,13 +15,17 @@ import org.springframework.restdocs.RestDocumentation;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class RestTrainingApplicationTests {
 
-  protected static final String BOOK_URL = "/v2/book";
+  protected static final String BOOK_URL = "/v2/book/";
 
   @Rule
   public final RestDocumentation
@@ -50,6 +58,48 @@ public class RestTrainingApplicationTests {
 	.andExpect(status().isOk())
 	.andExpect(jsonPath("$", hasSize(0)))
         .andDo(document("index"));
+  }
+
+  @Test
+  public void create() throws Exception {
+    Book2 book = new Book2();
+    book.setTitle("Ein");
+    book.setAuthor("Philipp");
+    book.setIsbn("1234");
+    book.setPages(100);
+    byte[] bookJson = toJson(book);
+
+    //CREATE
+    MvcResult result = mockMvc.perform(post(BOOK_URL)
+                               .content(bookJson)
+                               .contentType(MediaType.APPLICATION_JSON)
+                               .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isCreated())
+        .andReturn();
+    long id = getResourceIdFromUrl(result.getResponse().getRedirectedUrl());
+
+    //RETRIEVE
+    mockMvc.perform(get(BOOK_URL + id).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id", is((int) id)))
+        .andExpect(jsonPath("$.title", is(book.getTitle())))
+        .andExpect(jsonPath("$.author", is(book.getAuthor())))
+        .andExpect(jsonPath("$.isbn", is(book.getIsbn())))
+        .andExpect(jsonPath("$.pages", is(book.getPages())));
+
+    //DELETE
+    mockMvc.perform(delete(BOOK_URL + id))
+        .andExpect(status().isNoContent());
+  }
+
+  private long getResourceIdFromUrl(String locationUrl) {
+    String[] parts = locationUrl.split("/");
+    return Long.valueOf(parts[parts.length - 1]);
+  }
+
+  private byte[] toJson(Object r) throws Exception {
+    ObjectMapper map = new ObjectMapper();
+    return map.writeValueAsString(r).getBytes();
   }
 
 }
