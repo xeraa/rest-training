a759f4715774f7a2d7491a75031219515d1f2fb6
Philipp Krenn <pk@xeraa.net>
2015-10-22 23:45:36 +0200

    MINOR fixed confusing code

--- 03/src/main/java/net/xeraa/controller/NameController.java
+++ 03/src/main/java/net/xeraa/controller/NameController.java
@@ -5,21 +5,19 @@ import net.xeraa.model.Name;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-@Controller
+@RestController
 public class NameController {
 
   private static final String TEMPLATE = "Hello %s!";
 
   @RequestMapping("/hello")
-  @ResponseBody
   public HttpEntity<Name> nameGreeting(
       @RequestParam(value = "name", required = false, defaultValue = "Philipp") String name) {
 
